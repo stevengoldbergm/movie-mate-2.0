a77ce0a766fcd532725cdb697964ab43e300fe77
John Tash <jtash25@gmail.com>
2022-09-27 10:19:24 -0400

    Minor updates to the seed js file to include movie and review data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,27 @@
 const sequelize = require('../config/connection');
-const {User} = require('../models');
+const {User, Movie, Review} = require('../models');
 
 const userData = require('./userData.json');
-//const reviewData = require('./reviewData.json');
-//const movieData = require('./movieData.json');
+const reviewData = require('./reviewData.json');
+const movieData = require('./movieData.json');
 
 const seedDatabase = async() => {
     await sequelize.sync ({force:true});
 
-    const users = await User.create (userData,{
+    const users = await User.bulkCreate (userData,{
         individualHooks:true, 
         returning:true,
-});
+})
+    const movies = await Movie.bulkCreate (movieData,{
+    individualHooks:true, 
+    returning:true,
+}
+)
+const reviews = await Review.bulkCreate (reviewData,{
+    individualHooks:true, 
+    returning:true,
+}
+);
 process.exit(0);
 };
 

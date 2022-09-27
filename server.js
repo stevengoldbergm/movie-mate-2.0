c06f6a19dcbc30f899b0372fc0a1428802be95f0
SG_Demi <stevengoldbergm@gmail.com>
2022-09-27 13:32:01 -0500

    Added movieData object referencing to the movie-details endpoint

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 // Import express
 const express = require('express');
 const app = express();
-const path = require('path')
-const routes = require('./controllers')
-const helpers = require('./utils/helpers')
 
 // Define port value
 const PORT = process.env.PORT || 3001;;
@@ -25,11 +22,29 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     // res.send('index') // sends the index.html file from Public (not necessary anymore)
     // res.render('main', { layout: 'index' });
-    res.render('main', { search: true }); // I no longer have to specify the layout, since the default layout is set above! I can still set specific layouts if desired
+    res.render('signUp', { search: true }); // I no longer have to specify the layout, since the default layout is set above! I can still set specific layouts if desired
 });
 
-// Enables use of api routes
-app.use(routes);
+// Return movie details
+app.get('/movie-details', (req, res) => {
+    const movieData = {
+        title: "Movie Title Placeholder", 
+        actor: "Movie Actors Placeholder", 
+        director: "Movie Directors Placeholder",
+        mpaRating: "MPA Rating Placeholder",
+        releaseDate: "Movie Release Date Placeholder",
+        reviewScore: "Movie Review Score Placeholder",
+        userReviewScore: "Movie User Review Score Placeholder",
+        genre: "Movie Genre Placeholder",
+        writer: "Movie Writers Placeholder",
+        plotSummary: "Movie Plot Summary Placeholder",
+        posterURL: "https://via.placeholder.com/480x720",
+        ytEmbed: "https://www.youtube.com/embed/C0DPdy98e4c",
+    }
+    // res.send('index') // sends the index.html file from Public (not necessary anymore)
+    // res.render('main', { layout: 'index' });
+    res.render('movieDetails', { search: true, movieData }); // I no longer have to specify the layout, since the default layout is set above! I can still set specific layouts if desired
+});
 
 // Listen for the PORT
 app.listen(PORT, () => {

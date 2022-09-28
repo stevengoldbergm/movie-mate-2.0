857ebcbf59a231f8ed1cce4c8c64efd948750a76
Dave Strider <speedymota1234@gmail.com>
2022-09-28 11:06:25 -0400

    second commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -43,4 +43,22 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try{
+    const userData = await User.update(req.body,{
+      where: {
+        id:  req.params.id 
+      }
+    });  
+    if(!userData){
+      res.status(404).json({message:'No User Found With This ID'});
+      return;
+    }
+    
+    res.status(200).json(userData);
+  } catch(err){
+    res.status(500).json(err);
+  }
+
+})
 module.exports = router;

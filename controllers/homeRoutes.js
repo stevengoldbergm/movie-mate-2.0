75f6bd85bb6d7434bd59339b985700a76437646d
SG_Demi <stevengoldbergm@gmail.com>
2022-09-29 13:19:35 -0500

    Linked Youtube search to movie details

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/reviews/:id', (req, res) => {
 })
 
 router.get('/movie-details/:imdbID', async (req, res) => {
-    // movieData should be a fetch based on the omdbID param
+    // ---------- Search OMDB for data ---------- //
 
     // OMDB Key Variables
     const omdbSearch = 'https://www.omdbapi.com/?i=' // change t to s if you want a list of similar movie names
@@ -32,19 +32,45 @@ router.get('/movie-details/:imdbID', async (req, res) => {
 
     movieData = await axios.get(searchResult)
     movieData = movieData.data;
-    console.log(movieData) // Working
+    // console.log(movieData) // Working
 
     // Pull Rotten Tomatoes from movieData.Ratings
     rtScore = movieData.Ratings[1]
     if (rtScore) {
         rtScore = JSON.stringify(rtScore.Value)
+        rtScore = rtScore.substring(1, 4)
     } else {
         rtScore = "N/A"
     }
-
     // console.log(rtScore.Value) // Working
 
-    res.render('movieDetails', { search: true, movieDetails: true, movieData, rtScore }); 
+    // ---------- Search Youtube for trailer ---------- //
+
+    // Youtube Search Variables
+    const youTubeApiKey = 'AIzaSyArL85QacNinNMsTR0SLDijTFsPP8JkT0s'
+    // var youTubeApiKey = 'AIzaSyArL85QacNinNMsTR0SLDijTFsPP8JkT0s' // Steve's Key
+    const ytSearch = 'https://youtube.googleapis.com/youtube/v3/search?q='
+    const plusTrailer = " movie trailer"
+    const ytApiKey = '&key=AIzaSyArL85QacNinNMsTR0SLDijTFsPP8JkT0s'
+    const ytPart = '&part=snippet'
+    const ytType = '&type=video'
+    const ytResults = '&maxResults=1'
+    const ytEmbedBase = 'https://www.youtube.com/embed/'
+    const title = movieData.Title
+    const year = movieData.Year
+    // Define youtube search
+    var ytSearchResult = ytSearch + title + " " + year + plusTrailer + ytPart + ytType + ytResults + ytApiKey
+
+    // Fetch youtube data
+    const ytResult = await axios.get(ytSearchResult)
+    const ytEmbedId = ytResult.data.items[0].id.videoId
+    const ytEmbed = ytEmbedBase + ytEmbedId;
+
+    console.log('\n\ndata.items:',ytResult.data.items[0].id.videoId,'\n\n');
+    // console.log(ytResult);
+    // const ytEmbed = 
+
+    res.render('movieDetails', { search: false, movieDetails: true, movieData, rtScore, ytEmbed }); 
 });
 
 // User profile page
--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -1,70 +1,19 @@
-// ----------- Modal Controller ----------- //
+// ----------- Search Controller ----------- //
 
-// Add modal functionality 
-  // Make into a new page later
-  document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-      $el.classList.add('is-active');
-    }
-  
-    function closeModal($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllModals() {
-      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-        closeModal($modal);
-      });
-    }
-  
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
-    });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-  
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-      const e = event || window.event;
-  
-      if (e.keyCode === 27) { // Escape key
-        closeAllModals();
-      }
-    });
-  });
-  
-  // Activate the modal
-  document.querySelector("#srch-form").addEventListener('submit', modalSearch);
-  
-  async function modalSearch(event) {
+// Activate Search Form
+document.querySelector("#srch-form").addEventListener('submit', mainSearch);
+
+// Search Form Function
+async function mainSearch(event) {
     // Stop refresh 
     event.preventDefault();
-  
+
     // Pull the search data from OMDB
     const searchData = await searchOMDB();
-    console.log(searchData)
-  
-    // Turn the search data from OMDB into an array
-  
-    // Refresh page with the modal set to "is-active" 
-    
-    // Populate modal with links using the array data
-    document.querySelector(".modal").classList.add('is-active');
-  }
+    console.log(searchData);
+    postSearchResults(searchData);
+
+}
 
 // ---------- Search History Dropdown ---------- //
 
@@ -92,23 +41,95 @@ function fillHistory() {
 
 // Use search history buttons to enter search value, then pull movie data
 async function fillSearch (event) {
-  if(!event.target.textContent || (event.target.textContent.trim() == "Clear History")) {
-      return;
-  }
+    // Don't add "Clear History" to search bar on click!
+    if(!event.target.textContent || (event.target.textContent.trim() == "Clear History")) {
+        return;
+    }
 
-  // console.log(event) // Working
-  // console.log(event.target) // Working
-  // console.log(event.target.textContent) // Working
+    // console.log(event) // Working
+    // console.log(event.target) // Working
+    // console.log(event.target.textContent) // Working
 
-  searchEl.value = event.target.textContent;
-  const searchData = await searchOMDB();
-  console.log(searchData); // Working
+    if (!event.target.value) {
+        searchEl.value = event.target.textContent;
+    } 
+    const searchData = await searchOMDB();
+    console.log(searchData); // Working
 
-  // Select the table body
-  const table = document.querySelector('#table-body');
-  // Build out search results with DOM
-  searchData.forEach((movie) => {
+    postSearchResults(searchData);
+//     // Generate results on screen
+//     // Select Elements
+//     const resultTableEl = document.querySelector('#search-results')
+//     const table = document.querySelector('#table-body');
+    
+//     // Remove all child elements if they exist
+//     removeChildren(table);
+
+//     // Reveal results table
+//     resultTableEl.classList.remove('is-hidden');
+
+//     // Build out search results with DOM
+//     searchData.forEach((movie) => {
+//     console.log('Starting')
+
+//     // console.log(movie) // working
+//     const {Title, Year, imdbID} = movie;
+//     // console.log(Title, Year, imdbID); // Working
+
+//     // Create row element
+//     const rowEl = document.createElement('tr');
+//     console.log(rowEl);
+
+//     // create spacer element
+//     const spacerEl = document.createElement('td');
+//         spacerEl.style.width = "5%";
+//     const spacerImage = document.createElement('i');
+//         spacerImage.classList.add('fa', 'fa-bell-o');
+
+//     spacerEl.append(spacerImage);
+
+//     // Create Title element
+//     const titleEl = document.createElement('td');
+//         titleEl.innerHTML = Title;
+
+//     // Create Year element
+//     const yearEl = document.createElement('td');
+//         yearEl.innerHTML = `Released: ${Year}` 
+
+//     // Create redirect button
+//     const buttonBoxEl = document.createElement('td');
+//         buttonBoxEl.classList.add('level-right');
+//     const buttonEl = document.createElement('a');
+//         buttonEl.classList.add('button', 'is-small', 'is-info');
+//         buttonEl.setAttribute('imdb-id', imdbID);
+//         buttonEl.innerHTML = 'View Movie Details';
+//         buttonEl.setAttribute('href', `/movie-details/${imdbID}`)
+
+//     buttonBoxEl.append(buttonEl);
+    
+//     // Append row to table
+//     rowEl.append(spacerEl, titleEl, yearEl, buttonBoxEl);
+//     table.append(rowEl);
+//   })
+}
+
+// Post search results
+function postSearchResults(searchData) {
+        // Generate results on screen
+    // Select Elements
+    const resultTableEl = document.querySelector('#search-results')
+    const table = document.querySelector('#table-body');
+    
+    // Remove all child elements if they exist
+    removeChildren(table);
+
+    // Reveal results table
+    resultTableEl.classList.remove('is-hidden');
+
+    // Build out search results with DOM
+    searchData.forEach((movie) => {
     console.log('Starting')
+
     // console.log(movie) // working
     const {Title, Year, imdbID} = movie;
     // console.log(Title, Year, imdbID); // Working
@@ -148,7 +169,13 @@ async function fillSearch (event) {
     rowEl.append(spacerEl, titleEl, yearEl, buttonBoxEl);
     table.append(rowEl);
   })
-  
+}
+
+// Function to remove child elements
+function removeChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 // Function to clear local storage/search history
@@ -205,7 +232,7 @@ async function searchOMDB() {
     console.log(dropDownMenuContent)
 
     dropDownMenuContent.prepend(newLink);
-}
+    }
 
   // Return list of movie objects based on search parameters
   return response.data.Search;
